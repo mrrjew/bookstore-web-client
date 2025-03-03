a1844b7eef6d4acb85f639ad470d3f62c27dc1bf
mrr.jew <100996700+mrrjew@users.noreply.github.com>
2025-03-03 19:31:34 +0000

    refactor(cart): add functionality

--- app/(pages)/(home)/Books.tsx
+++ app/(pages)/(home)/Books.tsx
@@ -1,16 +1,17 @@
 "use client";
-import React, { useRef } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-import { BiSolidChevronLeft, BiSolidChevronRight } from 'react-icons/bi';
-import { books } from '@/app/data/books';
-import Book from '@/app/components/Book';
-import { Pagination, Navigation, Mousewheel } from 'swiper/modules';
+import React, { useRef } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import type { SwiperRef } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { BiSolidChevronLeft, BiSolidChevronRight } from "react-icons/bi";
+import { books } from "@/app/data/books";
+import Book from "@/app/components/Book";
+import { Pagination, Navigation, Mousewheel } from "swiper/modules";
 
 export default function Books() {
-  const swiperRef = useRef(null);
+  const swiperRef = useRef<SwiperRef>(null);
 
   return (
     <div className="my-24 relative">
@@ -21,13 +22,13 @@ export default function Books() {
       <div className="hidden absolute top-[45%] z-10 sm:flex items-center justify-between w-full">
         <div
           className="bg-white shadow-lg cursor-pointer rounded-full mx-2 font-thin p-4 custom-prev"
-          onClick={() => swiperRef?.current !== null && swiperRef?.current?.swiper.slidePrev()}
+          onClick={() => swiperRef.current?.swiper?.slidePrev()}
         >
           <BiSolidChevronLeft className="text-3xl text-gray-700" />
         </div>
         <div
           className="bg-white shadow-lg cursor-pointer rounded-full mx-2 font-thin p-4 custom-next"
-          onClick={() => swiperRef?.current?.swiper?.slideNext()}
+          onClick={() => swiperRef.current?.swiper?.slideNext()}
         >
           <BiSolidChevronRight className="text-3xl text-gray-700" />
         </div>
@@ -36,6 +37,7 @@ export default function Books() {
       <Swiper
         ref={swiperRef}
         spaceBetween={14}
+        mousewheel={{ forceToAxis: true }}
         slidesPerView={3}
         breakpoints={{
           0: {
@@ -51,9 +53,8 @@ export default function Books() {
             spaceBetween: 14,
           },
         }}
-        pagination={{ clickable: true, el: '.custom-pagination' }}
-        navigation={{ nextEl: '.custom-next', prevEl: '.custom-prev' }}
-        mousewheel={{ forceToAxis: true }}
+        pagination={{ clickable: true, el: ".custom-pagination" }}
+        navigation={{ nextEl: ".custom-next", prevEl: ".custom-prev" }}
         modules={[Pagination, Navigation, Mousewheel]}
         className="relative my-8 w-screen h-max"
       >
